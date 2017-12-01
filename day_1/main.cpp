be58d5a6e4fce44184f1934001fee7fdabf8d116
Matthew Matheson <mmatheson@uber.com>
2017-11-30 22:38:15 -0800

    working

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -2,13 +2,25 @@
 #include <fstream>
 
 int main() {
-  std::ifstream inputStream;
-  inputStream.open("../input/intput.txt");
+  std::ifstream inputStream("input/input.txt");
+  std::string str((std::istreambuf_iterator<char>(inputStream)),
+                 std::istreambuf_iterator<char>());
 
-  std::string inputString;
-  while(inputStream >> inputString) {
-    std::cout << inputString << std::endl;
+  size_t sum = 0;
+
+  for(size_t i = 0; i < str.size() - 1; ++i) {
+   if(str.at(i) == str.at(i + 1)) {
+     std::cout << str.at(i) << " = " << str.at(i + 1) << std :: endl;
+     sum += static_cast<uint8_t>(str.at(i) - '0');
+   }
   }
 
-  std::cout << std::endl << "END" << std::endl;
+  if(str.at(0) == str.at(str.size() - 1)) {
+    std::cout << str.at(0) << " = " << str.at(str.size() - 1) << std :: endl;
+    sum += str.at(0) - '0';
+  } else {
+    std::cout << str.at(0) << " ne " << str.at(str.size() - 1) << std :: endl;
+  }
+
+  std::cout << "SUM: " << std::to_string(sum) << std::endl;
 }
