a5a8f6193ab9e98bf973568ce01546cb8262fb8b
Matthew Matheson <matt.matheson@outlook.com>
2017-12-03 17:59:10 -0800

    day 2

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <limits>
+#include <cassert>
 
 int main(int argc, char** argv) {
+  if(argc != 2) {
+    std::cout << "Expected 1 arg, got " << std::to_string(argc - 1) << std::endl;
+    return 1;
+  }
+
+  std::string filename(argv[1]);
+  std::cout << "Opening File: " << filename << std::endl;
+
+  std::ifstream inputStream(filename);
+  if(!inputStream.is_open()) {
+    std::cout << "Failed to open file: " << filename << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  unsigned int checksum = 0;
+  while(std::getline(inputStream, line)) {
+    std::stringstream linestream(line);
+    unsigned int min = std::numeric_limits<unsigned int>::max();
+    unsigned int max = 0;
+
+    std::string word;
+    while(linestream >> word) {
+      unsigned int val = std::stoi(word);
+      min = std::min(val, min);
+      max = std::max(val, max);
+    }
+
+    checksum += (max - min);
+  }
+
+  std::cout << "Checksum: " << checksum << std::endl;
 }
